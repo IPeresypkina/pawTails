50593027576a16afbaf549bed072f88227ad1af0
IPeresypkina <iperesypkina9@gmail.com>
2020-05-11 13:43:01 +0300

    console

--- config/console.php
+++ config/console.php
@@ -39,22 +39,9 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environments
-//    $config['bootstrap'][] = 'gii';
-//    $config['modules']['gii'] = [
-//        'class' => 'yii\gii\Module',
-//    ];
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['*'],
-    ];
-
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['*'],
     ];
 }
 
